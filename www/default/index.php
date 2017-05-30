f42311377fa026f2d821be7778a0d2f55af12d85
Jose Maria Hidalgo Garcia <jhidalgo3@gmail.com>
2017-05-30 17:38:41 -0500

    Example SOAP Client

--- www/default/index.php
+++ www/default/index.php
@@ -1,1 +1,17 @@
-<?php phpinfo();
+<html>
+<h1>Docker SOAP Example</h1>
+Is soap installed? <?php echo class_exists('\SoapClient') ? "Yes" : "No"; ?>
+<h2>Working Example</h2>
+<?php $sillyNumber = is_numeric($_GET['degrees']) ? $_GET['degrees'] : 0; ?>
+How much is <?php echo $sillyNumber ?> °F in °C? <?php
+// convert number using example service from http://www.webservicex.net/WS/WSDetails.aspx?WSID=31&CATID=13
+$client = new \SoapClient('http://www.webservicex.net/ConvertTemperature.asmx?WSDL');
+$args = [
+    'Temperature' => $sillyNumber,
+    'FromUnit' => 'degreeFahrenheit',
+    'ToUnit' => 'degreeCelsius'
+];
+
+echo $client->ConvertTemp($args)->ConvertTempResult; ?> °C
+
+<small>Add ?degrees=&lt;double&gt; to change the requested value</small>
